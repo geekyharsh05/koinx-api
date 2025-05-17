0608df95b54b7e0ee36b20b48070528955c8be6b
Harsh <hp424805@gmail.com>
2025-05-17 09:35:35 +0530

    Add Swagger configuration for Crypto API documentation

---
+++ api-server/src/util/swagger.ts
@@ -0,0 +1,18 @@
+
+export const swaggerOptions = {
+    definition: {
+      openapi: "3.0.0",
+      info: {
+        title: "Crypto API",
+        version: "1.0.0",
+        description: "API for cryptocurrency statistics",
+      },
+      servers: [
+        {
+          url: `http://localhost:3000`,
+        },
+      ],
+    },
+    apis: ["./src/routes/*.ts"],
+  };
+  
